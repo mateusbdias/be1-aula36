29e7f3bf439fa9c2461bc5f7db39c4d476cdd6e4
Mateus Benites Dias <imattdias@gmail.com>
2022-04-06 10:30:55 -0300

    CTD - Back End I - Aula 36 - Exercício Sync

--- src/main/java/com/dh/hibernate/aula33/repository/TimeRepository.java
+++ src/main/java/com/dh/hibernate/aula33/repository/TimeRepository.java
@@ -2,8 +2,6 @@ package com.dh.hibernate.aula33.repository;
 
 import com.dh.hibernate.aula33.model.Time;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 
 public interface TimeRepository extends JpaRepository<Time, Integer> {
 
