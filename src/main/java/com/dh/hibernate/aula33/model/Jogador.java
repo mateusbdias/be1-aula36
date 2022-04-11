fec541efaefd4be661b8e06237b54f4450a91589
Mateus Benites Dias <imattdias@gmail.com>
2022-04-11 08:44:41 -0300

    CTD - Back End I - Aula 36

--- src/main/java/com/dh/hibernate/aula33/model/Jogador.java
+++ src/main/java/com/dh/hibernate/aula33/model/Jogador.java
@@ -1,7 +1,5 @@
 package com.dh.hibernate.aula33.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
 import javax.persistence.*;
 
 @Entity
@@ -15,7 +13,6 @@ public class Jogador {
     private String posicao;
     private Double peso;
     private Double altura;
-    // @JsonIgnore
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "time_id")
     private Time time;
